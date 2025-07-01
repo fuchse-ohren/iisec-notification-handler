28e650b58bbc653b31243f3a896132eba4542ca4
Super User <root@Functions.kitsunet.local>
2025-07-01 07:52:57 +0000

    起動用スクリプトを変更

--- iisec.sh
+++ iisec.sh
@@ -2,6 +2,7 @@
 cd $(dirname "$0")
 export IISEC_ID=""
 export IISEC_PW=""
-export GROQ_API_KEY=""
+export MODEL_PATH="./model/tinyswallow-1.5b-instruct-q8_0.gguf"
 export DISCORD_WEBHOOK=""
+export SLACK_WEBHOOK=""
 set | /usr/bin/python3 iisec.py | tee -a log.txt
